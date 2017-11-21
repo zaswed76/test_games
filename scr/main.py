cd7cc31d6aac79c5fb2c31b36d425b7a5a305547
Cassa <zaswed76@gmail.com>
2017-11-21 23:35:06 +0200

    add str

---
+++ scr/main.py
@@ -0,0 +1,8 @@
+
+
+def main():
+    print("main")
+    input()
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,23 @@
+from os.path import join, dirname
+
+import scr
+from setuptools import setup, find_packages
+
+setup(
+        name="add_table",
+        # в __init__ пакета
+        version=scr.__version__,
+        packages=find_packages(
+                exclude=["*.exemple", "*.exemple.*", "exemple.*",
+                         "exemple"]),
+        include_package_data=True,
+        long_description=open(
+                join(dirname(__file__), 'README.rst')).read(),
+        install_requires=[],
+        entry_points={
+            'console_scripts':
+                ['test_scr = scr.main:main']
+        }
+
+)
+
